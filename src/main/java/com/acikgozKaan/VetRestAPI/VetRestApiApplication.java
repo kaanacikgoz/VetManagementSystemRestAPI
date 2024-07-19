910e829408a349bc6bf94b9fb235967ddc337c7f
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-20 01:21:02 +0300

    Created Project

---
+++ src/main/java/com/acikgozKaan/VetRestAPI/VetRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.acikgozKaan.VetRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetRestApiApplication.class, args);
+	}
+
+}
