a79ec6d224f1ab92152779447171b4009dd09e95
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-21 04:17:43 +0300

    Added CRUD Methods To CustomerController

--- src/main/java/com/acikgozKaan/VetRestAPI/api/CustomerController.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/api/CustomerController.java
@@ -1,2 +1,63 @@
-package com.acikgozKaan.VetRestAPI.api;public class CustomerController {
+package com.acikgozKaan.VetRestAPI.api;
+
+import com.acikgozKaan.VetRestAPI.business.abstracts.ICustomerService;
+import com.acikgozKaan.VetRestAPI.core.modelMapper.IModelMapperService;
+import com.acikgozKaan.VetRestAPI.core.result.ResultData;
+import com.acikgozKaan.VetRestAPI.core.utilies.ResultHelper;
+import com.acikgozKaan.VetRestAPI.dto.request.customer.CustomerSaveRequest;
+import com.acikgozKaan.VetRestAPI.dto.request.customer.CustomerUpdateRequest;
+import com.acikgozKaan.VetRestAPI.dto.response.CustomerResponse;
+import com.acikgozKaan.VetRestAPI.entity.Customer;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/customers")
+public class CustomerController {
+
+    private final ICustomerService customerService;
+    private final IModelMapperService modelMapper;
+
+    public CustomerController(ICustomerService customerService, IModelMapperService modelMapper) {
+        this.customerService = customerService;
+        this.modelMapper = modelMapper;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResultData<CustomerResponse> save(@Valid @RequestBody CustomerSaveRequest customerSaveRequest) {
+        Customer saveCustomer = modelMapper.forRequest().map(customerSaveRequest, Customer.class);
+        this.customerService.save(saveCustomer);
+
+        CustomerResponse customerResponse = modelMapper.forResponse().map(saveCustomer, CustomerResponse.class);
+        return ResultHelper.created(customerResponse);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Customer> getAll() {
+        return customerService.getAll();
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CustomerResponse> update(@Valid @RequestBody CustomerUpdateRequest customerUpdateRequest) {
+        Customer updateCustomer = modelMapper.forRequest().map(customerUpdateRequest, Customer.class);
+        customerService.update(updateCustomer);
+
+        CustomerResponse customerResponse = modelMapper.forResponse().map(updateCustomer, CustomerResponse.class);
+        return ResultHelper.success(customerResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<Customer> delete(@PathVariable("id") Long id) {
+        Customer deletedCustomer = customerService.getById(id);
+        this.customerService.delete(id);
+        return ResultHelper.deleted(deletedCustomer);
+    }
+
 }
