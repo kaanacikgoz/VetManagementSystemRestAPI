f0b045898f4337d391ea80887b9ca1ff90c578cb
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-23 02:20:24 +0300

    Added query methods for handled duplicate data save and findByName

--- src/main/java/com/acikgozKaan/VetRestAPI/dao/CustomerRepo.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dao/CustomerRepo.java
@@ -4,6 +4,14 @@ import com.acikgozKaan.VetRestAPI.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByMail(String mail);
+    Optional<Customer> findByPhone(String phone);
+    List<Customer> findByName(String name);
+
 }
