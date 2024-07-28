b6a239ed4174e7a2fced7a3a3d80917310f6b6e6
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-28 18:59:32 +0300

    Added vaccine DTO requests

--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/vaccine/VaccineSaveRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/vaccine/VaccineSaveRequest.java
@@ -1,2 +1,35 @@
-package com.acikgozKaan.VetRestAPI.dto.request.vaccine;public class VaccineSaveRquest {
+package com.acikgozKaan.VetRestAPI.dto.request.vaccine;
+
+import com.acikgozKaan.VetRestAPI.entity.Animal;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VaccineSaveRequest {
+
+    @NotBlank(message = "Customer name cannot be null or empty")
+    private String name;
+
+    @NotBlank(message = "Customer name cannot be null or empty")
+    private String code;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate protectionStartDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate protectionFinishDate;
+
+    private List<Long> animalIds;
+
 }
--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/vaccine/VaccineUpdateRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/vaccine/VaccineUpdateRequest.java
@@ -1,2 +1,27 @@
-package com.acikgozKaan.VetRestAPI.dto.request.vaccine;public class VaccineUpdateRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.vaccine;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class VaccineUpdateRequest {
+
+    private String name;
+
+    private String code;
+
+    private LocalDate protectionStartDate;
+
+    private LocalDate protectionFinishDate;
+
+    private List<Long> animalIds;
+
 }
