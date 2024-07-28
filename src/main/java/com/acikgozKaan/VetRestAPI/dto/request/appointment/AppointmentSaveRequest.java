be934c3610ac70e967517dd4625337eda8079a4b
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-28 18:57:57 +0300

    Added appointments' DTO request

--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/appointment/AppointmentSaveRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/appointment/AppointmentSaveRequest.java
@@ -1,2 +1,27 @@
-package com.acikgozKaan.VetRestAPI.dto.request.appointment;public class AppointmentSaveRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.appointment;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentSaveRequest {
+
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime appointmentDate;
+
+    //private Long customerId; This should be come from animal entity
+
+    private Long animalId;
+
+    private Long doctorId;
+
 }
--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/appointment/AppointmentUpdateRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/appointment/AppointmentUpdateRequest.java
@@ -1,2 +1,24 @@
-package com.acikgozKaan.VetRestAPI.dto.request.appointment;public class AppointmentUpdateRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.appointment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentUpdateRequest {
+
+    private LocalDateTime appointmentDate;
+
+    //private Long customerId;
+
+    private Long animalId;
+
+    private Long doctorId;
+
 }
