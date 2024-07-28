cf006af90972e37b171399ba9a4633aec8853ce5
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-28 18:58:28 +0300

    Added availabledate's DTO request

--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/availableDate/AvailableDateSaveRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/availableDate/AvailableDateSaveRequest.java
@@ -1,2 +1,22 @@
-package com.acikgozKaan.VetRestAPI.dto.request.availableDate;public class AvailableDateSaveRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.availableDate;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailableDateSaveRequest {
+
+    private LocalDate availableDate;
+
+    private List<Long> doctorIds;
+
 }
--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/availableDate/AvailableDateUpdateRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/availableDate/AvailableDateUpdateRequest.java
@@ -1,2 +1,22 @@
-package com.acikgozKaan.VetRestAPI.dto.request.availableDate;public class AvailableDateUpdateRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.availableDate;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AvailableDateUpdateRequest {
+
+    private LocalDate availableDate;
+
+    private List<Long> doctorIds;
+
 }
