10026a790f5a1a752e18fa8cc2fd82eb89773922
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-07-28 18:59:09 +0300

    Added doctor DTO requests

--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/doctor/DoctorSaveRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/doctor/DoctorSaveRequest.java
@@ -1,2 +1,33 @@
-package com.acikgozKaan.VetRestAPI.dto.request.doctor;public class DoctorSaveRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.doctor;
+
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorSaveRequest {
+
+    private String name;
+
+    private String phone;
+
+    @Email(message = "Invalid email format")
+    private String mail;
+
+    private String address;
+
+    private String city;
+
+    private List<Long> availableDateIds;
+
+    //private List<Long> appointmentIds;
+
 }
--- src/main/java/com/acikgozKaan/VetRestAPI/dto/request/doctor/DoctorUpdateRequest.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/dto/request/doctor/DoctorUpdateRequest.java
@@ -1,2 +1,32 @@
-package com.acikgozKaan.VetRestAPI.dto.request.doctor;public class DoctorUpdateRequest {
+package com.acikgozKaan.VetRestAPI.dto.request.doctor;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DoctorUpdateRequest {
+
+    private String name;
+
+    private String phone;
+
+    @Email
+    private String mail;
+
+    private String address;
+
+    private String city;
+
+    private List<Long> availableDateIds;
+
+    //private List<Long> appointmentIds;
+
 }
