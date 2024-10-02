434b65c6c03c67f009a02826f833580daef7eb18
Kaan Acikgoz <kaanacikgoz@outlook.com>
2024-10-02 23:02:53 +0300

    Added builder annotation with lombok

--- src/main/java/com/acikgozKaan/VetRestAPI/entity/Appointment.java
+++ src/main/java/com/acikgozKaan/VetRestAPI/entity/Appointment.java
@@ -1,10 +1,7 @@
 package com.acikgozKaan.VetRestAPI.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +9,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "appointments")
 public class Appointment {
